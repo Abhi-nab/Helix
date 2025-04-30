58b712168178cbfcec4fec89e7ba82fe0e71c50c
Abhinab Kumar Nayak <abhinab161098@gmail.com>
2025-04-30 12:10:03 +0530

    Converted to single file and rest duplicates were deleted

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,4 @@
-
-    document.addEventListener("DOMContentLoaded", () => {
-    // Load header
-    fetch("assets/html/header.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
-      })
-      .catch(err => console.error("Header load error:", err));
-  
-    // Load footer
-    fetch("assets/html/footer.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("footer-placeholder").innerHTML = data;
-      })
-      .catch(err => console.error("Footer load error:", err));
-  });
-  
-    // Preloader
-  
-  
+ 
  
     window.addEventListener("load", () => {
       document.querySelector(".preloader").style.display = "none";
